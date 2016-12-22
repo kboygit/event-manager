d6cb7513c98bea872737fef47d11ff346528890e
kboygit <heybisayangdako@gmail.com>
2016-12-21 23:39:45 -0800

    update

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -38,7 +38,7 @@ def clean_zipcode(zipcode)
   if zipcode.nil?
     zipcode = "00000"
   elsif zipcode.length < 5
-     zipcode.rjust(5,"0")
+     zipcode.to_s.rjust(5,"0")[0..4]
   elsif zipcode.length > 5
     zipcode = zipcode[0..4]
   else
