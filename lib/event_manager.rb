cfdd738e293bb1475424e37f07b2b3cfb0624fd3
kboygit <heybisayangdako@gmail.com>
2016-12-21 19:17:41 -0800

    update

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,28 +1,30 @@
 puts "EventManager Initialized!"
 
-# ---- Another way of reading in as an array of lines ---- #
-# contents = File.read "event_attendees.csv"
-# puts contents
+    # ---- Another way of reading in as an array of lines ---- #
+    # contents = File.read "event_attendees.csv"
+    # puts contents
 
 
-# lines = File.readlines "event_attendees.csv"
-# lines.each do |line|
-#   columns = line.split(".")
-#   puts columns
-# end
+        # lines = File.readlines "event_attendees.csv"
+        # lines.each do |line|
+        #   columns = line.split(".")
+        #   puts columns
+        # end
 
-# #Accessing the arrays in each elements
-# lines = File.readlines "event_attendees.csv"
-# lines.each do |line|
-#   columns = line.split(".")
-#   name = columns[2]
-#   puts name
-# end
+            # #Accessing the arrays in each elements
+            # lines = File.readlines "event_attendees.csv"
+            # lines.each do |line|
+            #   columns = line.split(".")
+            #   name = columns[2]
+            #   puts name
+            # end
 
-lines = File.readlines "event_attendees.csv"
-lines.each do |line|
-  next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
-  columns = line.split(",")
-  name = columns[2]
-  puts name
-end
+
+                #
+                lines = File.readlines "event_attendees.csv"
+                lines.each do |line|
+                  next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
+                  columns = line.split(",")
+                  name = columns[2]
+                  puts name
+                end
