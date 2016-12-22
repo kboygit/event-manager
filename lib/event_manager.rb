ff672b5f7828c41d0da414e3e80a0df96a99bcc7
kboygit <heybisayangdako@gmail.com>
2016-12-21 18:03:33 -0800

    convert string into an array of columns

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,13 +1,12 @@
 puts "EventManager Initialized!"
 
 # ---- Another way of reading in as an array of lines ---- #
-# contents = File.read "event_attendees.csv" #
-# puts contents #
+# contents = File.read "event_attendees.csv"
+# puts contents
 
-
-puts "EventManager initialized."
 
 lines = File.readlines "event_attendees.csv"
 lines.each do |line|
-  puts line
+  columns = line.split(".")
+  puts columns
 end
