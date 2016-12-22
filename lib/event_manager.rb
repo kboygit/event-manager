680d8ab94606bcf74e27687a514189e737a67b6f
kboygit <heybisayangdako@gmail.com>
2016-12-21 19:17:12 -0800

    accessing only names in the arr

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -5,8 +5,24 @@ puts "EventManager Initialized!"
 # puts contents
 
 
+# lines = File.readlines "event_attendees.csv"
+# lines.each do |line|
+#   columns = line.split(".")
+#   puts columns
+# end
+
+# #Accessing the arrays in each elements
+# lines = File.readlines "event_attendees.csv"
+# lines.each do |line|
+#   columns = line.split(".")
+#   name = columns[2]
+#   puts name
+# end
+
 lines = File.readlines "event_attendees.csv"
 lines.each do |line|
-  columns = line.split(".")
-  puts columns
+  next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
+  columns = line.split(",")
+  name = columns[2]
+  puts name
 end
