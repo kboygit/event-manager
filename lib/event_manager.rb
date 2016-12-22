bb6dbced00968a29e4a09603ee0d937ee207a331
kboygit <heybisayangdako@gmail.com>
2016-12-21 19:42:44 -0800

    parsing in ruby

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,3 @@
-puts "EventManager Initialized!"
 
     # ---- Another way of reading in as an array of lines ---- #
     # contents = File.read "event_attendees.csv"
@@ -20,11 +19,23 @@ puts "EventManager Initialized!"
             # end
 
 
-                #
-                lines = File.readlines "event_attendees.csv"
-                lines.each do |line|
-                  next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
-                  columns = line.split(",")
-                  name = columns[2]
-                  puts name
-                end
+                # #A second way to solve this problem is to track the index of the current line.
+                # lines = File.readlines "event_attendees.csv"
+                #   row_index = 0
+                #   lines.each do |line|
+                #     row_index = row_index + 1
+                #     next if row_index == 1
+                #     columns = line.split(",")
+                #     name = columns[2]
+                #     puts name
+                #   end
+
+# Switching over to use the CSV Library# 
+require "csv"
+puts "EventManager Initialized!"
+
+contents = CSV.open "event_attendees.csv", headers: true
+contents.each do |row|
+  name = row[2]
+  puts name
+end
