7f5285f4735947e6e8930f00695fdd9992eacfaa
kboygit <heybisayangdako@gmail.com>
2016-12-22 00:42:46 -0800

    cleaning display of legislators added other CSV

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -50,8 +50,8 @@
 
 # Switching over to use the CSV Library #
 # Acessing Columns by their Names #
-require "csv"
-require "sunlight/congress"
+require 'csv'
+require 'sunlight/congress'
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
@@ -59,14 +59,22 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
 end
 
-puts "EventManager Initialized!"
-#able to output the individual and zipcode
-contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+puts "EventManager initialized."
+
+contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+
 contents.each do |row|
   name = row[:first_name]
+
   zipcode = clean_zipcode(row[:zipcode])
 
   legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
 
-puts "#{name} #{zipcode}"
+  legislator_names = legislators.collect do |legislator|
+    "#{legislator.first_name} #{legislator.last_name}"
+  end
+
+  legislators_string = legislator_names.join(", ")
+
+  puts "#{name} #{zipcode} #{legislators_string}"
 end
