13cd441237b62b34ca83fe8d4e87b391b96bf889
kboygit <heybisayangdako@gmail.com>
2016-12-22 01:27:48 -0800

    create method for form leter gen.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -50,6 +50,57 @@
 
 # Switching over to use the CSV Library #
 # Acessing Columns by their Names #
+# require 'csv'
+# require 'sunlight/congress'
+# require 'erb'
+#
+# Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+#
+# def clean_zipcode(zipcode)
+#   zipcode.to_s.rjust(5,"0")[0..4]
+# end
+#
+# def legislators_by_zipcode(zipcode)
+#   Sunlight::Congress::Legislator.by_zipcode(zipcode)
+# end
+#
+# puts "EventManager initialized."
+#
+# contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+#
+# template_letter = File.read "form_letter.erb"
+# erb_template = ERB.new template_letter
+#
+# # contents.each do |row|
+# #   name = row[:first_name]
+# #
+# #   zipcode = clean_zipcode(row[:zipcode])
+# #
+# #   legislators = legislators_by_zipcode(zipcode)
+# #
+# #   form_letter = erb_template.result(binding)
+# #   puts form_letter
+# # end
+# contents.each do |row|
+#   id = row[0]
+#   name = row[:first_name]
+#
+#   zipcode = clean_zipcode(row[:zipcode])
+#
+#   legislators = legislators_by_zipcode(zipcode)
+#
+#   form_letter = erb_template.result(binding)
+#
+#   Dir.mkdir("output") unless Dir.exists? "output"
+#
+#   filename = "output/thanks_#{id}.html"
+#
+#   File.open(filename,'w') do |file|
+#     file.puts form_letter
+#   end
+#
+# end
+
 require 'csv'
 require 'sunlight/congress'
 require 'erb'
@@ -64,6 +115,16 @@ def legislators_by_zipcode(zipcode)
   Sunlight::Congress::Legislator.by_zipcode(zipcode)
 end
 
+def save_thank_you_letters(id,form_letter)
+  Dir.mkdir("output") unless Dir.exists?("output")
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename,'w') do |file|
+    file.puts form_letter
+  end
+end
+
 puts "EventManager initialized."
 
 contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
@@ -71,32 +132,13 @@ contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :sy
 template_letter = File.read "form_letter.erb"
 erb_template = ERB.new template_letter
 
-# contents.each do |row|
-#   name = row[:first_name]
-#
-#   zipcode = clean_zipcode(row[:zipcode])
-#
-#   legislators = legislators_by_zipcode(zipcode)
-#
-#   form_letter = erb_template.result(binding)
-#   puts form_letter
-# end
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
-
   zipcode = clean_zipcode(row[:zipcode])
-
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
 
-  Dir.mkdir("output") unless Dir.exists? "output"
-
-  filename = "output/thanks_#{id}.html"
-
-  File.open(filename,'w') do |file|
-    file.puts form_letter
-  end
-
+  save_thank_you_letters(id,form_letter)
 end
